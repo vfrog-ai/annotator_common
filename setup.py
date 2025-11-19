5754d75dc2fd9f98336cbc2f02c0ba5b60adbdc7
Frédéric Leroy <1552991+fleroy91@users.noreply.github.com>
2025-11-19 14:07:49 +0400

    Bump version to 0.2.0 for Pub/Sub migration

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="annotator-common",
-    version="0.1.0",
+    version="0.2.0",
     author="vfrog",
     description="Common utilities and shared code for annotator services",
     long_description=long_description,
